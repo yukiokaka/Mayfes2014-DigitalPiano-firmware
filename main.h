d164ad896cf8ae683f1c5929834f4882394be9b1
takaito <taka.ito.yk@gmail.com>
2014-03-21 07:40:00 +0900

    録音中になり続けるバグ有り

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#define NOMMAL_MODE 0 /* 通常モード */
+#define NORMAL_MODE 0 /* 通常モード */
 #define REC_MODE 1 /* 録音モード */
 #define PLAY_MODE 2 /* 再生モード */
+
+#define REC_LIMIT 200
 
 int main (void);
 void SysTick_Handler (void);
